571469d41c37d6151beefeec306db72c325cbc01
Mario Di Cristofano <io@gmail.com>
2022-12-05 21:38:39 +0100

    Initial commit

---
+++ src/main/java/com/develhope/DeployEnvironmentsVariables/DeployEnvironmentsVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.develhope.DeployEnvironmentsVariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEnvironmentsVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEnvironmentsVariablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develhope/DeployEnvironmentsVariables/DeployEnvironmentsVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.DeployEnvironmentsVariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEnvironmentsVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
